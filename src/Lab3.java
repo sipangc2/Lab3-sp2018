8c186231f7e5f3dead92df817c3c8c1ce46b0ef1
sipangc2 <sipangc2@illinois.edu>
2018-02-07 20:35:48 -0600

    first push

--- src/Lab3.java
+++ src/Lab3.java
@@ -2,13 +2,16 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.Scanner;
 
-/**
- * Retrieve contents from a URL and return them as a string.
- *
- * @param url url to retrieve contents from
- * @return the contents from the url as a string, or an empty string on error
- */
+
 public class Lab3 {
+
+        /**
+         * Retrieve contents from a URL and return them as a string.
+         *
+         * @param url url to retrieve contents from
+         * @return the contents from the url as a string, or an empty string on error
+         */
+
         public static String urlToString(final String url) {
                 Scanner urlScanner;
                 try {
@@ -20,5 +23,34 @@ public class Lab3 {
                 urlScanner.close();
                 return contents;
         }
-        System.out.println(urlToString("http://erdani.com/tdpl/hamlet.txt"));
+
+        public static int countwords(final String strurl) {
+                int word = 0;
+                for (int i = 0; i < strurl.length(); i++) {
+                        if (!Character.isLetter(strurl.charAt(i))) {
+                            word++;
+                            if (!Character.isLetter(strurl.charAt(i+1))) {
+                                word--;
+                                if (strurl.charAt(i) == '.') {
+                                    word++;
+                                }
+                            }
+                        }
+                }
+                return word;
+        }
+
+        /**
+         *
+         * @param unused
+         */
+
+        public static void main(String[] unused) {
+            Scanner lineScanner = new Scanner(System.in);
+            System.out.println("Enter a url: ");
+            String inputurl = lineScanner.nextLine();
+            String stringurl = urlToString(inputurl);
+            System.out.println("The word count is: ");
+            System.out.println(countwords(stringurl));
+        }
 }
